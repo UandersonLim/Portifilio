b5e698b95541e7b65ede00ee4914f2f902ec143a
Uanderson <uanderson.lm@gmail.com>
2024-01-03 08:13:49 -0300

    Adicionando botão currículo

--- src/Components/Button_curriculum/index.jsx
+++ src/Components/Button_curriculum/index.jsx
@@ -4,7 +4,7 @@ export const Component_Button_Curriculum = () => {
   return (
     <a href="https://drive.google.com/file/d/1gi7wo_X-Q_IsDf0VjVZmq4gntrpqbmk4/view?usp=sharing" target="_blank">
       <Box_Button>
-        <img src="../../../public/assets/icones-linguagens/icone_download.png" />
+        <img src="assets/icones-linguagens/icone_download.png" />
       </Box_Button>
     </a>
   );
